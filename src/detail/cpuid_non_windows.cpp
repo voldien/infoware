c0d281f40cacd001f7f4e96eaa11df50edfae4c4
voldie <voldiekami@gmail.com>
2022-06-16 13:29:14 +0200

    Allow compilation of ARM arch

--- src/detail/cpuid_non_windows.cpp
+++ src/detail/cpuid_non_windows.cpp
@@ -12,21 +12,31 @@
 
 #ifndef _WIN32
 
-
 #include "infoware/detail/cpuid.hpp"
-#include <cpuid.h>
 #include <cstdint>
 
+#ifndef __arm__
+#include <cpuid.h>
 
 void iware::detail::cpuid(std::int32_t (&out)[4], std::int32_t x) {
 	__cpuid_count(x, 0, out[0], out[1], out[2], out[3]);
 }
-
 std::uint64_t iware::detail::xgetbv(std::uint32_t index) {
 	std::uint32_t eax, edx;
 	__asm__ __volatile__("xgetbv" : "=a"(eax), "=d"(edx) : "c"(index));
 	return ((uint64_t)edx << 32) | eax;
 }
+#else
+
+void iware::detail::cpuid(std::int32_t (&out)[4], std::int32_t x) {
+	out[0] = out[1] = out[2] = out[3] = 0;
+}
+
+std::uint64_t iware::detail::xgetbv(std::uint32_t index) {
+	return 0;
+}
+
+#endif
 
 
 #endif
